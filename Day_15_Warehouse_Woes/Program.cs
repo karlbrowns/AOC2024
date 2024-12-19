cef467387613d4eaed579ed66404cbf88f3f4c46
karlbrowns <simon.k.brrown@gmail.com>
2024-12-19 23:15:58 +0000

    Day 15 done

--- Day_15_Warehouse_Woes/Program.cs
+++ Day_15_Warehouse_Woes/Program.cs
@@ -206,6 +206,173 @@ void P1()
     Console.ReadLine();
 }
 
+int print_map(int[,] map, int width, int height)
+{
+    int result = 0;
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            switch (map[x, y])
+            {
+                case -1: Console.Write('#'); break;
+                case 0: Console.Write('.'); break;
+                case 1: Console.Write('['); result += 100 * y + x; break;
+                case 2: Console.Write(']'); break;
+                case 10: Console.Write('@'); break;
+            }
+        }
+        Console.WriteLine();
+    }
+    return result;
+}
+bool testup(int startx, int starty, int[,] map)
+{
+    bool result = true;
+    if (map[startx, starty-1] == -1) return false;
+    if (map[startx, starty - 1] == 0)
+    {
+        if (map[startx, starty] <= 2) map[startx, starty] += 4;
+        return true;
+    }
+    if ((map[startx, starty-1] & 3) == 1)
+    {
+        result = testup(startx, starty - 1, map);
+        if (result) result = testup(startx + 1, starty - 1, map);
+        if (result)
+        {
+            if (map[startx, starty] <= 2) map[startx, starty] += 4;
+        }
+    }
+    if ((map[startx, starty-1] & 3) == 2)
+    {
+        result = testup(startx, starty - 1, map);
+        if (result) result = testup(startx - 1, starty - 1, map);
+        if (result)
+        {
+            if (map[startx, starty] <= 2) map[startx, starty] += 4;
+        }
+    }
+    return result;
+}
+bool testdown(int startx, int starty, int[,] map)
+{
+    bool result = true;
+    if (map[startx, starty + 1] == -1) return false;
+    if (map[startx, starty + 1] == 0)
+    {
+        if (map[startx, starty] <= 2) map[startx, starty] += 4;
+        return true;
+    }
+    if ((map[startx, starty + 1] & 3) == 1)
+    {
+        result = testdown(startx, starty + 1, map);
+        if (result) result = testdown(startx + 1, starty + 1, map);
+        if (result)
+        {
+            if (map[startx,starty]<=2) map[startx, starty] += 4;
+        }
+    }
+    if ((map[startx, starty + 1] & 3) == 2)
+    {
+        result = testdown(startx, starty + 1, map);
+        if (result) result = testdown(startx - 1, starty + 1, map);
+        if (result)
+        {
+            if (map[startx, starty] <= 2) map[startx, starty] += 4;
+        }
+    }
+    return result;
+}
+bool moveup2(int startx, int starty, int[,] map, int width, int height)
+{
+    int y = starty;
+    if (map[startx, y - 1] == -1) return false;
+    if (map[startx, y - 1] == 0)
+    {
+        return true;
+    }
+    int block = 1;
+    bool can_move;
+    can_move = testup(startx, y - block, map);
+    if (can_move)
+    {
+        if ((map[startx, y - 1] & 3) == 1) can_move = testup(startx + 1, y - block, map);
+        if ((map[startx, y - 1] & 3) == 2) can_move = testup(startx - 1, y - block, map);
+    }
+    return can_move;
+    
+}
+bool movedown2(int startx, int starty, int[,] map, int width, int height)
+{
+    int y = starty;
+    if (map[startx, y + 1] == -1) return false;
+    if (map[startx, y + 1] == 0)
+    {
+        return true;
+    }
+    int block = 1;
+    bool can_move;
+    can_move = testdown(startx, y + block, map);
+    if (can_move)
+    {
+        if ((map[startx, y + 1] & 3) == 1) can_move = testdown(startx + 1, y + block, map);
+        if ((map[startx, y + 1] & 3) == 2) can_move = testdown(startx - 1, y + block, map);
+    }
+    return can_move;
+}
+void moveright2(int startx, int starty, int[,] map, out int endx, out int endy)
+{
+    endx = startx; endy = starty;
+    int x = startx;
+    if (map[x + 1, starty] == -1) return;
+    if (map[x + 1, starty] == 0)
+    {
+        endx = x + 1;
+        return;
+    }
+    int block = 1;
+    if (map[x + block, starty] >= 1)
+    {
+        while (map[x + block, starty] >= 1) block++;
+        if (map[x + block, starty] == -1) return;
+        if (map[x + block, starty] == 0)
+        {
+            endx = x + 1;
+            for (int steps = block; steps > 0; steps--)
+            {
+                map[x + steps, starty] = map[x + steps - 1, starty];
+            }
+            return;
+        }
+    }
+}
+void moveleft2(int startx, int starty, int[,] map, out int endx, out int endy)
+{
+    endx = startx; endy = starty;
+    int x = startx;
+    if (map[x - 1, starty] == -1) return;
+    if (map[x - 1, starty] == 0)
+    {
+        endx = x - 1;
+        return;
+    }
+    int block = 1;
+    if (map[x - block, starty] >= 1)
+    {
+        while (map[x - block, starty] >= 1) block++;
+        if (map[x - block, starty] == -1) return;
+        if (map[x - block, starty] == 0)
+        {
+            endx = x - 1;
+            for (int steps = block; steps > 0; steps--)
+            {
+                map[x - steps, starty] = map[x - steps + 1, starty];
+            }
+            return;
+        }
+    }
+}
 void P2()
 {
     int result = 0;
@@ -214,7 +381,7 @@ void P2()
     int height = 50;
     String data = "input.txt";
     List<string> input = read_input(data);
-    int[,] map = new int[width, height];
+    int[,] map = new int[width*2, height];
     int startx = 0, starty = 0;
     string instructions = "";
     for (int i = 0; i < input.Count; i++)
@@ -229,15 +396,16 @@ void P2()
             case 0:
                 for (int x = 0; x < input[i].Length; x++)
                 {
-                    if (input[i][x] == '#') map[x, i] = -1;
+                    if (input[i][x] == '#') { map[x<<1, i] = -1; map[(x<<1) + 1, i] = -1; }
                     if (input[i][x] == '@')
                     {
-                        startx = x;
+                        startx = x<<1;
                         starty = i;
-                        map[x, i] = 0;
+                        map[(x<<1), i] = 0;
+                        map[(x<<1) + 1, i] = 0;
                     }
-                    if (input[i][x] == 'O') map[x, i] = 1;
-                    if (input[i][x] == '.') map[x, i] = 0;
+                    if (input[i][x] == 'O') { map[x<<1, i] = 1; map[(x<<1) + 1, i] = 2; }
+                    if (input[i][x] == '.') { map[x<<1, i] = 0; map[(x<<1) + 1, i] = 0; }
                 }
                 break;
             case 1:
@@ -245,13 +413,93 @@ void P2()
                 break;
         }
     }
+    bool test;
+    int endx, endy;
+    width *= 2;
+    for (int i=0; i<instructions.Length; i++)
+    {
+        switch (instructions[i])
+        {
+            case '^':
+                Console.WriteLine("Up");
+                test = moveup2(startx, starty, map, width, height);
+                if (test)
+                {
+                    for (int k = 0; k < height; k++)
+                    {
+                        for (int j = 0; j < width; j++)
+                        {
+                            if (map[j, k] > 2)
+                            {
+                                map[j, k - 1] = map[j, k] - 4;
+                                map[j, k] = 0;
+                            }
+                        }
+                    }
+                }
+                if (!test)
+                {
+                    for (int k = 0; k < height; k++)
+                    {
+                        for (int j = 0; j < width; j++)
+                        {
+                            if (map[j, k] > 2) map[j, k] -= 4;
+                        }
+                    }
+                }
+                if (test) starty--;
+                break;
+            case '>':
+                Console.WriteLine("Right");
+                moveright2(startx, starty, map, out endx, out endy);
+                startx = endx; starty = endy;
+                break;
+            case 'v':
+                Console.WriteLine("Down");
+                test = movedown2(startx, starty, map, width, height);
+                if (test)
+                {
+                    for (int k = height - 1; k >= 0; k--)
+                    {
+                        for (int j = 0; j < width; j++)
+                        {
+                            if (map[j, k] > 2)
+                            {
+                                map[j, k + 1] = map[j, k] - 4;
+                                map[j, k] = 0;
+                            }
+                        }
+                    }
+                }
+                if (!test)
+                {
+                    for (int k = 0; k<height; k++)
+                    {
+                        for (int j=0; j<width; j++) {
+                            if (map[j, k] > 2) map[j, k] -= 4;
+                        }
+                    }
+                }
+
+                if (test) starty++;
+                break;
+            case '<':
+                Console.WriteLine("Left");
+                moveleft2(startx, starty, map, out endx, out endy);
+                startx = endx; starty = endy;
+                break;
+        }
+        //result = print_map(map, width, height);
+        //Console.ReadLine();
+    }
+    result = print_map(map, width, height);
     Console.WriteLine(result);
     Console.ReadLine();
 }
 
 Stopwatch t = new Stopwatch();
 t.Start();
-P1();
+//P1();
 t.Stop();
 Console.WriteLine("P1 took " + t.ElapsedMilliseconds/1000.0 + " seconds");
 t.Restart();
---
+++ Day_19_Linen_Layout/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+List<string> read_input(string name)
+{
+    List<string> strings = new List<string>();
+    foreach (string line in System.IO.File.ReadLines(name))
+    {
+        strings.Add(line);
+    }
+    return strings;
+}
+
+void P1()
+{
+    int result = 0;
+    int index = 0;
+    String data = "input.txt";
+    List<string> input = read_input(data);
+    for (int i=0; i<input.Count; i++)
+    {
+    }
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+void P2()
+{
+    int result = 0;
+    int index = 0;
+    String data = "input.txt";
+    List<string> input = read_input(data);
+    for (int i = 0; i < input.Count; i++)
+    {
+    }
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+Stopwatch t = new Stopwatch();
+t.Start();
+P1();
+t.Stop();
+Console.WriteLine("P1 took " + t.ElapsedMilliseconds/1000.0 + " seconds");
+t.Restart();
+P2();
+t.Stop();
+Console.WriteLine("P2 took " + t.ElapsedMilliseconds / 1000.0 + " seconds");
